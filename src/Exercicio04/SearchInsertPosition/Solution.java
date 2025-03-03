abf096b56674944341516297daf9dcc5d8901b70
MaiconDev <haelyton149@gmail.com>
2025-03-02 22:59:00 -0300

    Exercicio04-Search Insert Position

---
+++ src/Exercicio04/SearchInsertPosition/Solution.java
@@ -0,0 +1,21 @@
+package Exercicio04.SearchInsertPosition;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int esquerda = 0;
+        int direita = nums.length - 1;
+
+        while (esquerda <= direita) {
+            int meio = esquerda + (direita - esquerda) / 2;
+
+            if (nums[meio] == target) {
+                return meio;
+            }else if (nums[meio] < target) {
+                esquerda = meio + 1;
+            }else {
+                direita = meio - 1;
+            }
+        }
+        return esquerda;
+    }
+}
